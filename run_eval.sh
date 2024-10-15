d1a37e1feb1e9517f3f3023248a30d462bcd8c74
chengyuehuang511 <chuang475@gatech.edu>
2024-10-15 14:20:14 -0400

    new vizwiz

--- run_eval.sh
+++ run_eval.sh
@@ -28,7 +28,7 @@ echo $HOSTNAMES
 cd /coc/testnvme/chuang475/projects/VQA-ICL
 
 export PYTHONPATH="$PYTHONPATH:open_flamingo"
-srun --cpu_bind=v --accel-bind=gn /coc/testnvme/chuang475/miniconda3/envs/lavis_same/bin/python evaluate.py \
+srun -u /coc/testnvme/chuang475/miniconda3/envs/lavis_same/bin/python -m torch.distributed.run --nproc_per_node=8 evaluate.py \
     --vision_encoder_path ViT-L-14 \
     --vision_encoder_pretrained openai\
     --lm_path anas-awadalla/mpt-1b-redpajama-200b \
@@ -37,8 +37,8 @@ srun --cpu_bind=v --accel-bind=gn /coc/testnvme/chuang475/miniconda3/envs/lavis_
     --checkpoint_path "/nethome/chuang475/flash/.cache/huggingface/hub/models--openflamingo--OpenFlamingo-3B-vitl-mpt1b/snapshots/ed3a0c3190b2fc2d1c39630738896d4e73ce1bbc/checkpoint.pt" \
     --results_file "results.json" \
     --precision amp_bf16 \
-    --batch_size 32 \
-    --eval_vizwiz \
+    --batch_size 16 \
+    --eval_ok_vqa \
     --vqav2_train_image_dir_path "/srv/datasets/coco/train2014" \
     --vqav2_train_annotations_json_path "/srv/datasets/vqa2.0/v2_mscoco_train2014_annotations.json" \
     --vqav2_train_questions_json_path "/srv/datasets/vqa2.0/v2_OpenEnded_mscoco_train2014_questions.json" \
@@ -52,13 +52,13 @@ srun --cpu_bind=v --accel-bind=gn /coc/testnvme/chuang475/miniconda3/envs/lavis_
     --ok_vqa_test_annotations_json_path "/srv/datasets/ok-vqa_dataset/mscoco_val2014_annotations.json" \
     --ok_vqa_test_questions_json_path "/srv/datasets/ok-vqa_dataset/OpenEnded_mscoco_val2014_questions.json" \
     --textvqa_image_dir_path "/coc/pskynet4/chuang475/projects/vlm_robustness/tmp/datasets/textvqa/val/train_images/" \
-    --textvqa_train_questions_json_path "/coc/pskynet4/chuang475/projects/vlm_robustness/tmp/datasets/textvqa/train/train_questions_vqa_format.json" \
-    --textvqa_train_annotations_json_path "/coc/pskynet4/chuang475/projects/vlm_robustness/tmp/datasets/textvqa/train/train_annotations_vqa_format.json" \
-    --textvqa_test_questions_json_path "/coc/pskynet4/chuang475/projects/vlm_robustness/tmp/datasets/textvqa/val/val_questions_vqa_format.json" \
-    --textvqa_test_annotations_json_path "/coc/pskynet4/chuang475/projects/vlm_robustness/tmp/datasets/textvqa/val/val_annotations_vqa_format.json" \
-    --vizwiz_train_image_dir_path "/srv/datasets/vizwiz/data/Images/" \
-    --vizwiz_test_image_dir_path "/srv/datasets/vizwiz/data/Images/" \
-    --vizwiz_train_questions_json_path "/coc/pskynet4/chuang475/projects/vlm_robustness/tmp/datasets/vizwiz/train/train_questions_vqa_format.json" \
-    --vizwiz_train_annotations_json_path "/coc/pskynet4/chuang475/projects/vlm_robustness/tmp/datasets/vizwiz/train/train_annotations_vqa_format.json" \
-    --vizwiz_test_questions_json_path "/coc/pskynet4/chuang475/projects/vlm_robustness/tmp/datasets/vizwiz/val/val_questions_vqa_format.json" \
-    --vizwiz_test_annotations_json_path "/coc/pskynet4/chuang475/projects/vlm_robustness/tmp/datasets/vizwiz/val/val_annotations_vqa_format.json" \
+    --textvqa_train_questions_json_path "data/textvqa/train_questions_vqa_format.json" \
+    --textvqa_train_annotations_json_path "data/textvqa/train_annotations_vqa_format.json" \
+    --textvqa_test_questions_json_path "data/textvqa/val_questions_vqa_format.json" \
+    --textvqa_test_annotations_json_path "data/textvqa/val_annotations_vqa_format.json" \
+    --vizwiz_train_image_dir_path "/coc/pskynet4/chuang475/projects/vlm_robustness/tmp/datasets/vizwiz/images/train" \
+    --vizwiz_test_image_dir_path "/coc/pskynet4/chuang475/projects/vlm_robustness/tmp/datasets/vizwiz/images/val" \
+    --vizwiz_train_questions_json_path "data/vizwiz/train_questions_vqa_format.json" \
+    --vizwiz_train_annotations_json_path "data/vizwiz/train_annotations_vqa_format.json" \
+    --vizwiz_test_questions_json_path "data/vizwiz/val_questions_vqa_format.json" \
+    --vizwiz_test_annotations_json_path "data/vizwiz/val_annotations_vqa_format.json" \
